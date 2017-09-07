18354e7c3976501c5ebfd6484d2459923d862b89
Ciara Bujanos <c.buj22@gmail.com>
2017-09-07 13:41:15 -0600

    initial knex setup

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/club_reads',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/club_reads',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/test/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+  },
+};
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+// const path = require('path');
 const app = express();
 const port = (process.env.PORT || 3000);
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,54 +15,72 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // ENDPOINTS
 
 // Get the clubs so a new user can select which club to join
-app.get('/api/v1/club', () => {
-
-})
+app.get('/api/v1/club', (request, response) => {
+  database('club').select()
+    .then((club) => {
+      response.status(200).json(club);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 // Sign up a new user
-app.post('/api/v1/user', () => {
+app.post('/api/v1/user', (request, response) => {
 
-})
+});
 
 // Login a user
-app.post('/api/v1/user', () => {
+app.post('/api/v1/user', (request, response) => {
 
-})
+});
 
 // View all club books
-app.get('/api/v1/book', () => {
-
-})
+app.get('/api/v1/book', (request, response) => {
+  database('book').select()
+    .then((book) => {
+      response.status(200).json(book);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 // Add a new book to club books
-app.post('/api/v1/book', () => {
+app.post('/api/v1/book', (request, response) => {
 
-})
+});
 
 // Add a vote
-app.post('/api/v1/vote', () => {
+app.post('/api/v1/vote', (request, response) => {
 
-})
+});
 
 // Delete a vote
-app.delete('/api/v1/vote', () => {
+app.delete('/api/v1/vote', (request, response) => {
 
-})
+});
 
 // Get all comments
-app.get('/api/v1/comment', () => {
-
-})
+app.get('/api/v1/comment', (request, response) => {
+  database('comment').select()
+    .then((comment) => {
+      response.status(200).json(comment);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 // Edit comment
-app.patch('/api/v1/comment', () => {
+app.patch('/api/v1/comment', (request, response) => {
 
-})
+});
 
 // Post a comment
-app.post('/api/v1/comment', () => {
+app.post('/api/v1/comment', (request, response) => {
 
-})
+});
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
