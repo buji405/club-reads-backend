9fbd2d81279acb7be5ba1c4dd33a286260251934
Dave W <dviper@gmail.com>
2017-09-07 14:44:12 -0600

    Update db schema

--- db/migrations/20170907134038_initial.js
+++ db/migrations/20170907134038_initial.js
@@ -1,51 +1,51 @@
 
 exports.up = (knex, Promise) => Promise.all([
-  knex.schema.createTable('clubs', (table) => {
+  knex.schema.createTable('club', (table) => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').unique();
 
     table.timestamps(true, true);
   }),
 
-  knex.schema.createTable('users', (table) => {
+  knex.schema.createTable('user', (table) => {
     table.increments('id').primary();
-    table.string('email');
+    table.string('email').unique();
     table.integer('club_id').unsigned();
-    table.foreign('club_id').references('clubs.id');
+    table.foreign('club_id').references('club.id');
 
     table.timestamps(true, true);
   }),
 
-  knex.schema.createTable('books', (table) => {
+  knex.schema.createTable('book', (table) => {
     table.increments('id').primary();
     table.string('title');
     table.string('author');
-    table.string('isbn');
+    table.string('ISBN').unique();
     table.string('description');
     table.string('image');
     table.integer('upvotes');
     table.integer('downvotes');
     table.string('status');
     table.integer('user_id').unsigned();
-    table.foreign('user_id').references('users.id');
+    table.foreign('user_id').references('user.id');
 
     table.timestamps(true, true);
   }),
 
-  knex.schema.createTable('votes', (table) => {
+  knex.schema.createTable('vote', (table) => {
     table.increments('id').primary();
     table.integer('user_id').unsigned();
-    table.foreign('user_id').references('users.id');
+    table.foreign('user_id').references('user.id');
     table.integer('book_id').unsigned();
-    table.foreign('book_id').references('books.id');
+    table.foreign('book_id').references('book.id');
 
     table.timestamps(true, true);
   }),
 ]);
 
 exports.down = (knex, Promise) => Promise.all([
-  knex.schema.dropTable('clubs'),
-  knex.schema.dropTable('users'),
-  knex.schema.dropTable('books'),
-  knex.schema.dropTable('votes'),
+  knex.schema.dropTable('club'),
+  knex.schema.dropTable('user'),
+  knex.schema.dropTable('book'),
+  knex.schema.dropTable('vote'),
 ]);
