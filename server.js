9e3e7afa2cbb0a25015454f7bb280a83af2403eb
Ciara Bujanos <c.buj22@gmail.com>
2017-09-06 18:19:33 -0600

    add airbNb linter

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = (process.env.PORT || 3000);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
 });
 
-module.exports = app
+module.exports = app;
