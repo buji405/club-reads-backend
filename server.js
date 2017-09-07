416394bf7622cac4b3b5fc405878995bf6286c9b
Ciara Bujanos <c.buj22@gmail.com>
2017-09-07 09:45:19 -0600

    set up initial endpoints

--- server.js
+++ server.js
@@ -6,7 +6,59 @@ const port = (process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// ENDPOINTS
+
+// Get the clubs so a new user can select which club to join
+app.get('/api/v1/club', () => {
+
+})
+
+// Sign up a new user
+app.post('/api/v1/user', () => {
+
+})
+
+// Login a user
+app.post('/api/v1/user', () => {
+
+})
+
+// View all club books
+app.get('/api/v1/book', () => {
+
+})
+
+// Add a new book to club books
+app.post('/api/v1/book', () => {
+
+})
+
+// Add a vote
+app.post('/api/v1/vote', () => {
+
+})
+
+// Delete a vote
+app.delete('/api/v1/vote', () => {
+
+})
+
+// Get all comments
+app.get('/api/v1/comment', () => {
+
+})
+
+// Edit comment
+app.patch('/api/v1/comment', () => {
+
+})
+
+// Post a comment
+app.post('/api/v1/comment', () => {
+
+})
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
