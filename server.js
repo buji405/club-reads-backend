4b91abff5ee1bb5e27caaa91157151720cd564f6
Travis <tlgreg86@gmail.com>
2017-09-09 09:52:58 -0600

    Change localhost server from 3000 to 4000 for proxy on frontend.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const path = require('path');
 const app = express();
-const port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 4000);
 
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
