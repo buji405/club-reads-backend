612b7e361ecec921599ae20ba5ed25aa16f6e3f5
Ciara Bujanos <c.buj22@gmail.com>
2017-09-17 14:49:22 -0600

    Fix post vote and create delete vote tests

--- test/vote.spec.js
+++ test/vote.spec.js
@@ -19,30 +19,50 @@ describe('API vote routes', () => {
       .then(() => done())
       .catch(error => console.log(error));
   });
-  
+
   describe('POST /api/v1/vote', () => {
     it('should create a new user vote', (done) => {
       chai.request(server)
         .post('/api/v1/vote')
         .send({
-          id: 3,
+          id: 6,
           direction: 'up',
-          user_id: 1,
+          user_id: 4,
           book_id: 1,
         })
         .end((err, res) => {
           res.should.have.status(201);
           res.body.should.be.a('object');
           res.body.should.have.property('id');
-          res.body.id.should.equal(3);
+          res.body.id.should.equal(6);
           res.body.should.have.property('direction');
           res.body.direction.should.equal('up');
           res.body.should.have.property('user_id');
-          res.body.user_id.should.equal(1);
+          res.body.user_id.should.equal(4);
           res.body.should.have.property('book_id');
           res.body.book_id.should.equal(1);
           done();
-        })
-    })
-  })
-})
+        });
+    });
+  });
+  describe('DELETE /api/v1/vote/3', () => {
+    it('should delete a vote', (done) => {
+      chai.request(server)
+        .delete('/api/v1/vote/3')
+        .end((err, res) => {
+          console.log(res.body.vote);
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.vote[0].should.have.property('id');
+          res.body.vote[0].id.should.equal(3);
+          res.body.vote[0].should.have.property('direction');
+          res.body.vote[0].direction.should.equal('down');
+          res.body.vote[0].should.have.property('user_id');
+          res.body.vote[0].user_id.should.equal(1);
+          res.body.vote[0].should.have.property('book_id');
+          res.body.vote[0].book_id.should.equal(2);
+          done();
+        });
+    });
+  });
+});
